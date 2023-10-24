ce7f824d1d00a3d72b5d3cd73dcade6ed1ef65a1
ThomasLiang123 <tliang1850@github.com>
2023-10-24 20:32:41 +0800

    initial commit

---
+++ flyengine-opengl/flyengine-opengl/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  flyengine-opengl
+//
+//  Created by Thomas Liang on 10/24/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
